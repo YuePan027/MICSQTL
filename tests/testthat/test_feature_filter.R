4c0664157e49de0f6adc81d8e876fa0aa3413863
Yue <yuep027@gmail.com>
2023-04-21 12:53:39 -0500

    update tests

--- tests/testthat/test_feature_filter.R
+++ tests/testthat/test_feature_filter.R
@@ -4,7 +4,7 @@ se@metadata <- list(SNP_data = MICSQTL::SNP_data[,rev(seq_len(ncol(MICSQTL::SNP_
 test_that("feature filter function gives error for unmatched samples", {
     expect_error(feature_filter(se, target_protein = c("ABCA1"),
                                 filter_method = c("allele")), 
-                 "Samples in protein_data do not match that in SNP_data.")
+                 "Samples in protein_data do not match that in SNP_data")
 })
 
 
@@ -12,5 +12,5 @@ se@metadata <- list(SNP_data = MICSQTL::SNP_data, anno_SNP = MICSQTL::anno_SNP[1
 test_that("feature filter function gives error for unmatched annotation", {
     expect_error(feature_filter(se, target_protein = c("ABCA1"),
                                 filter_method = c("allele")), 
-                 "SNPs contained in annotation data frame `anno_SNP` must match the SNPs in `SNP_data`.")
+                 "SNPs contained in annotation data frame `anno_SNP` must match the SNPs in `SNP_data`")
 })
